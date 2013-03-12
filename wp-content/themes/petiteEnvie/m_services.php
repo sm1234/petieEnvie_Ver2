38c23b0f250472d2ae6dbcf7244d938d324c2c50
Jayant Sethi <thejayantsethi@gmail.com>
2013-03-12 14:50:16 +0530

    mobile version catalogue linked with wordpress
    specify 'm-catalogue' as tag of the post, and the hex code for the color
    for the text below the image.

--- wp-content/themes/petiteEnvie/m_services.php
+++ wp-content/themes/petiteEnvie/m_services.php
@@ -20,126 +20,34 @@ require_once 'mobile_header.php';
 </nav>
 
 <div id="page">
-
-
-<div class="c_blue panel" style="height: 350px;">
+<?php
+$catalogue=new WP_Query();
+$catalogue->query('tag=m-catalogue&order=ASC');
+if ($catalogue->have_posts()) : 
+while ( $catalogue->have_posts() ) : $catalogue->the_post();
+$postid = get_the_ID();
+$post_thumbnail_id = get_post_thumbnail_id();
+$feat_img = wp_get_attachment_url($post_thumbnail_id);
+?>
+<img src="<?php echo $feat_img;  ?>">
+<?php
+$all_the_tags = get_the_tags();
+if ($all_the_tags){
+foreach($all_the_tags as $this_tag) {
+if (strpos($this_tag->name, '#', 0) === 0) {
+?>
+<div class="panel" style="background-color : <?php echo $this_tag->name; ?>">
+	<p class="intro"><?php the_title(); ?></p>
+	<p><?php the_content(); ?></p>	
 </div>
-<div class="c_blue panel">
-	<p class="intro">Do you take Petite Envie
-	to be your perfect wedding
-	partner?</p>
-	<p>Well you should. A once in a lifetime day
-	should be complemented by a once in a lifetime
-	euphoric experience. Pickled Walnut will
-	prepare your day months before it happens,
-	taking the pressure of making the necessary
-	arrangements for the venue, trimmings and
-	catering from you – ensuring all you have to
-	think about is each other. Whatever style of
-	wedding you wish for, however small, however
-	large, you can be sure we’ll make it as unique
-	and as special as your personality.</p>	
 </div>
-
-<div class="c_purple panel" style="height: 350px;">
-</div>
-
-<div class="c_purple panel">
-	<p class="intro">Every event is different.<br>
-	Each brief is unique.</p>
-	<p>The tiniest detail is as important as the largest
-	– and to us, your event will be treated as if
-	it were a Royal visit – FYI, we can do that
-	too Ma’am. From organising, preparing for
-	and delivering a great event, to adding the
-	sparkle, the spice and the spit roast, the Pickled
-	Walnut team will take care of the whole
-	shebang. Simply, we never let you down with
-	the fresh food we prepare for your event, so
-	you can rest assured our event management
-	style will complement it beautifully.
-	Enough said!</p>	
-</div>
-
-<div class="c_light-blue panel" style="height: 350px;">
-</div>
-
-<div class="c_light-blue panel">
-	<p class="intro">Fancy second helpings?
-	Most of our corporate
-	clients do.</p>
-	<p>Pickled Walnut caters for a smorgasbord
-	of venues and sites across the UK
-	including; Tennis Tournaments/Sporting
-	events, Product Launches, Team building
-	events/Company on site events, Gala/
-	Annual dinners, Conference catering, Award
-	Ceremonies and Charity Fund raisers.
-	Enough said!</p>	
-	<p>It’s a fact, getting to know our customers
-	is key to a successful result, which is why
-	we enjoy repeat bookings with leading
-	events companies and direct clients. Also
-	fact, 95% of our corporate customers use
-	our services for a second time, enjoying
-	once again our expertise whilst confi dent
-	in the knowledge that we can deliver and
-	exceed their expectations every time.</p>
-</div>
-
-<div class="c_pink panel" style="height: 350px;">
-</div>
-
-<div class="c_pink panel">
-	<p class="intro">Fancy second helpings?
-	Most of our corporate
-	clients do.</p>
-	<p>Pickled Walnut caters for a smorgasbord
-	of venues and sites across the UK
-	including; Tennis Tournaments/Sporting
-	events, Product Launches, Team building
-	events/Company on site events, Gala/
-	Annual dinners, Conference catering, Award
-	Ceremonies and Charity Fund raisers.
-	Enough said!</p>	
-	<p>It’s a fact, getting to know our customers
-	is key to a successful result, which is why
-	we enjoy repeat bookings with leading
-	events companies and direct clients. Also
-	fact, 95% of our corporate customers use
-	our services for a second time, enjoying
-	once again our expertise whilst confi dent
-	in the knowledge that we can deliver and
-	exceed their expectations every time.</p>
-</div>
-
-<div class="c_purple panel" style="height: 350px;">
-</div>
-
-<div class="c_purple panel">
-	<p class="intro">Fancy second helpings?
-	Most of our corporate
-	clients do.</p>
-	<p>Pickled Walnut caters for a smorgasbord
-	of venues and sites across the UK
-	including; Tennis Tournaments/Sporting
-	events, Product Launches, Team building
-	events/Company on site events, Gala/
-	Annual dinners, Conference catering, Award
-	Ceremonies and Charity Fund raisers.
-	Enough said!</p>	
-	<p>It’s a fact, getting to know our customers
-	is key to a successful result, which is why
-	we enjoy repeat bookings with leading
-	events companies and direct clients. Also
-	fact, 95% of our corporate customers use
-	our services for a second time, enjoying
-	once again our expertise whilst confi dent
-	in the knowledge that we can deliver and
-	exceed their expectations every time.</p>
-</div>
-
-</div><!-- #page -->
+<?php
+}
+}
+}
+endwhile;
+endif;
+?>
 
 </div>
 <?php 
