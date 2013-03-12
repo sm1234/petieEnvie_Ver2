4e7d4baa527a89b2ec922cf5c990a1ca37d46c8c
Jayant Sethi <thejayantsethi@gmail.com>
2013-03-12 16:06:10 +0530

    script in all secondary menu templates moved to a seperate file
    script in all secondary menu templates moved to a seperate file

--- wp-content/themes/petiteEnvie/tea_cakes.php
+++ wp-content/themes/petiteEnvie/tea_cakes.php
@@ -46,36 +46,12 @@ $curr++;
 endwhile;
 endif;
 ?>
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$("header").hide();
-	$("#peBody").removeClass();
-	$("#peBody").addClass("services");
-	var container_width_secondary = jQuery('.container').width();
-	if(jQuery(window).width()<775)
-		{
-			jQuery('#c_secondary > li > a').css('padding','2px 5px 3px');
-		}
-	else
-		{
-			jQuery('#c_secondary > li > a').css('padding','2px 15px 3px');
-		}
-	var navigationHeight = jQuery('.navbar').height();
-	jQuery('body').css('padding-top', navigationHeight);	
-	$("#c_catalogue").addClass("active");
-	var acticeElement = window.location.href.split('/')[4];
-	$("a[rel="+acticeElement+"]").parent().addClass("active");
-	var $current2 = $('#c_secondary > .active');
-	if($current2.length==0)
-	{
-	$("#c_secondary").hide().delay("300").slideDown(400);
-	}
-	else
-	{
-	$("#c_secondary").show();
-	}
-});
-</script>
+
+<?php
+wp_register_script('jkl', get_template_directory_uri().'/catalogue-script.js',array('jquery'));
+wp_enqueue_script('jkl');
+?>
+
 <?php
 get_footer();
 ?>
